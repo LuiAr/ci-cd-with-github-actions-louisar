a773f96c904591c705b3f05c7a4e5309ecfabf4e
LuiAr <louis.arbey@efrei.net>
2024-01-11 14:17:16 +0100

    Final Version

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -17,11 +17,6 @@ class TestAppE2E(unittest.TestCase):
         self.driver = webdriver.Chrome(options=chrome_options)
         self.driver.get('http://localhost:5000')
 
-    # def wait_for_element(self, by, value, timeout=10):
-    #     return WebDriverWait(self.driver, timeout).until(
-    #         EC.presence_of_element_located((by, value))
-    #     )
-
     def test_add_and_delete_item(self):
         self.driver.get("http://127.0.0.1:5000/")
         self.driver.find_element(By.NAME, "item").click()
@@ -37,20 +32,3 @@ class TestAppE2E(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
-
-
-
-    # def test_add_and_delete_item(self):
-    #     # Test adding an item
-    #     self.wait_for_element(By.NAME, "item").click()
-    #     self.driver.find_element(By.NAME, "item").send_keys("Test Item")
-    #     self.driver.find_element(By.CSS_SELECTOR, "button").click()
-    #     self.wait_for_element(By.NAME, "new_item").click()
-    #     self.driver.find_element(By.NAME, "new_item").send_keys("Modified Item")
-    #     self.driver.find_element(By.CSS_SELECTOR, "li button").click()
-    #     self.wait_for_element(By.NAME, "item").click()
-    #     self.driver.find_element(By.NAME, "item").send_keys("A new Item")
-    #     self.driver.find_element(By.CSS_SELECTOR, ".container > form > button").click()
-    #     self.wait_for_element(By.LINK_TEXT, "Delete").click()
-    #     self.wait_for_element(By.LINK_TEXT, "Delete").click()

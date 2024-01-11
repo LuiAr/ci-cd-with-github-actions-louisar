bd891a5dac273f25897cc1e47effdc3cdabb6e19
LuiAr <louis.arbey@efrei.net>
2024-01-11 14:15:17 +0100

    Update CI/CD workflow and Dockerfile

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -3,34 +3,33 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-import time
-
-def waitAbit():
-    time.sleep(1)
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import chromedriver_autoinstaller_fix
 
 class TestAppE2E(unittest.TestCase):
     def setUp(self):
         chrome_options = Options()
-        chrome_options.add_argument("--headless")  
-        chrome_options.add_argument("--no-sandbox") 
-        chrome_options.add_argument("--disable-dev-shm-usage") 
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chromedriver_autoinstaller_fix.install()  
         self.driver = webdriver.Chrome(options=chrome_options)
         self.driver.get('http://localhost:5000')
+
+    # def wait_for_element(self, by, value, timeout=10):
+    #     return WebDriverWait(self.driver, timeout).until(
+    #         EC.presence_of_element_located((by, value))
+    #     )
 
     def test_add_and_delete_item(self):
         self.driver.get("http://127.0.0.1:5000/")
         self.driver.find_element(By.NAME, "item").click()
-        waitAbit()
         self.driver.find_element(By.NAME, "item").send_keys("AddItem")
-        waitAbit()
         self.driver.find_element(By.NAME, "item").send_keys(Keys.ENTER)
-        waitAbit()
         self.driver.find_element(By.NAME, "new_item").click()
-        waitAbit()
         self.driver.find_element(By.NAME, "new_item").send_keys("UpdateItem")
-        waitAbit()
         self.driver.find_element(By.NAME, "new_item").send_keys(Keys.ENTER)
-        waitAbit()
         self.driver.find_element(By.LINK_TEXT, "Delete").click()
 
     def tearDown(self):
@@ -38,3 +37,20 @@ class TestAppE2E(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
+
+
+
+    # def test_add_and_delete_item(self):
+    #     # Test adding an item
+    #     self.wait_for_element(By.NAME, "item").click()
+    #     self.driver.find_element(By.NAME, "item").send_keys("Test Item")
+    #     self.driver.find_element(By.CSS_SELECTOR, "button").click()
+    #     self.wait_for_element(By.NAME, "new_item").click()
+    #     self.driver.find_element(By.NAME, "new_item").send_keys("Modified Item")
+    #     self.driver.find_element(By.CSS_SELECTOR, "li button").click()
+    #     self.wait_for_element(By.NAME, "item").click()
+    #     self.driver.find_element(By.NAME, "item").send_keys("A new Item")
+    #     self.driver.find_element(By.CSS_SELECTOR, ".container > form > button").click()
+    #     self.wait_for_element(By.LINK_TEXT, "Delete").click()
+    #     self.wait_for_element(By.LINK_TEXT, "Delete").click()
